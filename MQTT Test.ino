8553bbe155e5cfea21e5d6b508a4c073b8a40e0a
BILLY Maxime <59318604+Oseliurs@users.noreply.github.com>
2020-01-30 19:46:18 +0100

    Create MQTT Test.ino

---
+++ MQTT Test.ino
@@ -0,0 +1,66 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* ssid = "Livebox-68D7"; // Enter your WiFi name
+const char* mqttServer = "rutaceae.ddns.net";
+const int mqttPort = 1883;
+const char* mqttUser = "user";
+const char* mqttPassword = "Ov96Hf66&*";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+
+  Serial.begin(115200);
+
+  WiFi.begin(ssid);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.publish("esp/test", "hello"); //Topic name
+  client.subscribe("esp/test");
+ 
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+ 
+void loop() {
+  client.loop();
+}
