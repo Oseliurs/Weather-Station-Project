c96c04c2ccada15c4e48516c37001a7afc45782c
BILLY Maxime <59318604+Oseliurs@users.noreply.github.com>
2020-01-30 20:58:37 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -4,3 +4,32 @@ This is the main file for the ESP8266 based Weather Station
 
 */
 
+/------------------------------------------------------------------------------------------
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+
+
+/------------------------------------------------------------------------------------------
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+/------------------------------------------------------------------------------------------
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+/------------------------------------------------------------------------------------------
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
